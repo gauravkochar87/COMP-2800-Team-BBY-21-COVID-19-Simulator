85ac38406410e4c68cd5f4fe3174daee8decd145
Winston Zhao <zhaoweicheng0824@gmail.com>
2020-05-15 15:15:04 -0700

    Move form to the left under map

--- scripts/main.js
+++ scripts/main.js
@@ -295,14 +295,3 @@ update.addEventListener('submit', (e) => {
 ////                    MAP FUNCTION                       ////
 ///////////////////////////////////////////////////////////////
 
-var map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 49.283832198,
-            lng: -123.119332856
-        },
-        zoom: 15
-    });
-}
