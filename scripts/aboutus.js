d6887c44cdebb03588d08d3679ae2e85b1376054
lynnoj <jonnylu@live.ca>
2020-05-15 00:46:18 -0700

    updated avatar and added info modals when clicking the read more button

---
+++ scripts/aboutus.js
@@ -0,0 +1,46 @@
+$(document).ready(function()
+{
+    // Gaurav
+    $("#gaurav-read-more").click(function()
+    {
+        $("#gaurav-modal").fadeIn();
+    });
+
+    $(".info-modal").click(function()
+    {
+        $("#gaurav-modal").fadeOut();
+    });
+
+    // Winston
+    $("#winston-read-more").click(function()
+    {
+        $("#winston-modal").fadeIn();
+    });
+
+    $(".info-modal").click(function()
+    {
+        $("#winston-modal").fadeOut();
+    });
+
+    // Jeff
+    $("#jeff-read-more").click(function()
+    {
+        $("#jeff-modal").fadeIn();
+    });
+
+    $(".info-modal").click(function()
+    {
+        $("#jeff-modal").fadeOut();
+    });
+
+    // Jonny
+    $("#jonny-read-more").click(function()
+    {
+        $("#jonny-modal").fadeIn();
+    });
+
+    $(".info-modal").click(function()
+    {
+        $("#jonny-modal").fadeOut();
+    });
+})
